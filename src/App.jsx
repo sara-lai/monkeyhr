f2862f0e3777f20eb682f050876f71613e42324a
sara-lai <shihualai@gmail.com>
2025-06-04 00:42:13 +0800

    add ReportFull to flow after setTimeout on procesing; get 33 test/7 category mock data from json

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,39 @@ import './App.css'
 
 import GithubForm from './components/GithubForm/GithubForm'
 import Processor from './components/Processor/Processor'
+import ReportFull from './components/ReportFull/ReportFull'
 
 import * as GithubService from './services/GithubService' // should put this in Landing isntead?
 import * as AirtableService from './services/AirtableService'
+
+import mockReportData from './services/mockProcessor.json';
+
 
 function App() {
 
   const [repoURL, setRepoURL] = useState('')
   const [projectType, setProjectType] = useState('...')
   const [repoData, setRepoData] = useState({})
+
+  // todo switching to Routes will probably make this unnecessary
+  const [preProcess, setPreProcess] = useState(true)
   const [processing, setProcessing] = useState(false)
+  const [newReportReady, setnewReportReady] = useState(false)
 
   async function kickoffProcessing(query) {
     setRepoURL(query)
     setProcessing(true)
+    setPreProcess(false)
 
     let repoData = await GithubService.getRepoBasics(query)
     setProjectType(repoData.language) // or dont use
     setRepoData(repoData)  
+
+    // usage from https://javascript.info/async-await
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000))
+
+    setnewReportReady(true)
+    setProcessing(false)
   }  
 
   console.log('hello repo!', repoData)
@@ -29,9 +44,11 @@ function App() {
 
   // function getRepoBasics() {
   // }
+
+  const reportData = 
   
   function processCommits() {
-    // live here or in landing? 
+    // live here or in landing?   
     GithubService.getCommits()
 
     // generate a report & update state??
@@ -41,9 +58,12 @@ function App() {
     <div className='landing-box'>
       <h1>MonkeyHR</h1>
 
-      {!processing && <GithubForm handleFormSubmit={kickoffProcessing} />}
+      {preProcess && <GithubForm handleFormSubmit={kickoffProcessing} />}
 
       {processing && <Processor repoURL={repoURL} repoData={repoData} />}
+
+      {newReportReady && <ReportFull data={mockReportData} />}
+
 
       {/* <Dashboard /> */}
 
---
+++ src/components/ReportFull/ReportFull.jsx
@@ -0,0 +1,9 @@
+const ReportFull = (props) => {
+
+    return (
+        <h1> This is the report page</h1>
+    )
+
+}
+
+export default ReportFull
