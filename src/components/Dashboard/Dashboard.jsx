ce5d6c5f3ba3da09c87131d815c8c08051a916d2
sara-lai <shihualai@gmail.com>
2025-06-05 15:10:23 +0800

    Card for latestReport on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,15 +2,36 @@ import { useState, useEffect } from 'react'
 
 import * as AirtableService from '../../services/AirtableService'
 
-import { Card, Divider, CardContent } from '@mui/material';
+import { Card, Divider, CardContent, Box } from '@mui/material';
+import FlagIcon from '@mui/icons-material/Flag';
 
 import './Dashboard.css'
 
+// todo -  currently dashboard is getting all data from airtable, but to get latest report probably needs props!
+
+const LatestReportSummary = () => {
+  const categories = ['Commits', 'Iteration', 'Code Choices', 'Code Style', 'Artefacts', 'AI Assessment']
+
+  return (
+    <Card sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 8, minHeight: 300 }}>
+      <div className='categories-flags'>
+        {categories.map((category) => (
+          <div className='category-flag-set'>
+            <span>{category}</span>
+            <FlagIcon sx={{ color: 'green' }} /> 
+          </div>
+        ))}
+      </div>
+    </Card>    
+  )
+}
+
 // put in own folder later??
+// ok the sx={{}} is getting ridiculous .... is this worth it? what's wrong with css?
 const ReportSummary = (props) => {
   return (
     <>
-      <Card sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', p: 2, minHeight: 80, mb: 2, width: '100%' }}>
+      <Card sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', p: 2, minHeight: 60, mb: 2, width: '100%', transition: 'transform 0.2s, box-shadow 0.2s', '&:hover': { transform: 'translateY(-2px)', boxShadow: 6, cursor: 'pointer', }, '&:focus': {outline: '2px solid #90caf9'} }}>
         <span>{props.report.fields.RepoURL}</span>
         <span>{props.report.fields.ProjectType}</span>        
         <button>view report</button>
@@ -40,9 +61,10 @@ const Dashboard = () => {
         <div className='dashboard-wrapper'>
           <div className='new-report-section'>
             <h3>Latest Report</h3>
+            <LatestReportSummary />
           </div>
           <div className='previous-report-section'>
-            <h3>Previous Reports</h3>
+            <h3>All Reports</h3>
             <div className='reports-summary-box'>
               {allReports.map(report => (
                   <ReportSummary report={report} />
--- src/components/Processor/Processor.jsx
+++ src/components/Processor/Processor.jsx
@@ -1,3 +1,5 @@
+import './Processor.css'
+
 const ProcessingScreen = (props) => {
 
     function formatDate(date_raw){
@@ -6,14 +8,17 @@ const ProcessingScreen = (props) => {
     }
 
     return (
-        <div className="processing-container">
+        <div className="processing-wrapper">
             <h2>Processsing.... (estimated time: 2 minutes)</h2>
             <h2>{props.repoURL} </h2>
             <p>Project type: {props.repoData.language}, Created: {formatDate(props.repoData.created_at)}</p>
-            <p>Analyzing commits (5+ factors)...</p>
-            <p>Assessing iteration (4+ factors)...</p>
-            <p>Reviewing code choices (4+ factors)...</p>
+            <p>Reviewing commits...</p>
+            <p>Reviewing iteration...</p>
+            <p>Reviewing code choices...</p>
+            <p>Reviewing code style...</p>
+            <p>Reviewing artefacts...</p>
             <p>Cross-checking with AI...</p>
+            <p>Checking for mitigating factors...</p>
         </div>
     )
 
