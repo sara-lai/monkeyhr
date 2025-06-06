8ef5fdab460a990b3e1ae9971c8b2d59476c7f00
sara-lai <shihualai@gmail.com>
2025-06-07 00:22:00 +0800

    highlight stages on processing screen attempt 1

--- src/components/Processor/Processor.jsx
+++ src/components/Processor/Processor.jsx
@@ -1,5 +1,7 @@
 import './Processor.css'
 
+import { useState, useEffect } from 'react'
+
 const ProcessingScreen = (props) => {
 
     function formatDate(date_raw){
@@ -7,18 +9,28 @@ const ProcessingScreen = (props) => {
         return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
     }
 
+    // tricky feature
+    // goal: walk through the steps 1-by-1 at some interval ....setInterval.... 
+    // goal2: show a "." after each test next to the category, have those also put on page
+    // react: setInterval is a side effect, so use useEffect.... 
+    const [activeStage, setActiveStage] = useState(0) // match the indexes of stages
+    const stages = ['commits', 'iteration', 'code-choices', 'code-style', 'artefacts', 'ai-assessment', 'mitigation']
+    useEffect({
+        
+    }, [])
+
     return (
         <div className="processing-wrapper">
             <h2>Processsing.... (estimated time: 2 minutes)</h2>
             <h2>{props.repoURL} </h2>
             <p>Project type: {props.repoData.language}, Created: {formatDate(props.repoData.created_at)}</p>
-            <p>Reviewing commits...</p>
-            <p>Reviewing iteration...</p>
-            <p>Reviewing code choices...</p>
-            <p>Reviewing code style...</p>
-            <p>Reviewing artefacts...</p>
-            <p>Cross-checking with AI...</p>
-            <p>Checking for mitigating factors...</p>
+            <p className={activeStage === 'commits' ? 'active' : ''}>Reviewing commits</p>
+            <p className={activeStage === 'iteration' ? 'active' : ''}>Reviewing iteration</p>
+            <p className={activeStage === 'code-choices' ? 'active' : ''}>Reviewing code choices</p>
+            <p className={activeStage === 'code-style' ? 'active' : ''}>Reviewing code style</p>
+            <p className={activeStage === 'artefacts' ? 'active' : ''}>Reviewing artefacts</p>
+            <p className={activeStage === 'ai-assessment' ? 'active' : ''}>Cross-checking with AI</p>
+            <p className={activeStage === 'mitigation' ? 'active' : ''}>Checking mitigating factors</p>
         </div>
     )
 
