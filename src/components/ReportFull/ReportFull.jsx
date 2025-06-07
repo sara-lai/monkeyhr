3a3d4fb095df6eddc6ea38924679fafbc90e1fc2
sara-lai <shihualai@gmail.com>
2025-06-07 21:06:41 +0800

    fix report rendering

--- src/components/ReportFull/ReportFull.jsx
+++ src/components/ReportFull/ReportFull.jsx
@@ -1,20 +1,25 @@
 import { useParams } from 'react-router';
 
+// todo - a GET to airtable may be more appropriate than passing allReports and filtering by id.
+
 const ReportFull = (props) => {
 
     const { reportId } = useParams() // see pokemon lab
 
     const report = props.allReports.find(report => report.id === reportId)
 
-    console.log("here at reportFull", props.allReports)
-
+    let reportDataObj = {}
+    if (report){
+        reportDataObj = JSON.parse(report.fields.ReportData)
+    }
+    
     return (
         <>
-            {Object.keys(report.fields).map(category => (
+            {Object.keys(reportDataObj).map(category => (
                 <div className='category-set'>
                     <h3>{category}</h3>
                     <div className='tests-set'>
-                        {props.data[category].map(test => (
+                        {reportDataObj[category].map(test => (
                             <div className='test-block'>
                                 <p>{test.description}</p>
                                 <p>result: flag-icon</p>
